ee6e340c3835b34414f0bb308be0bee90278f65f
Priscilla Celine Setiawan <priscilla.setiawan@tomoloyalty.com>
2024-04-22 14:16:32 +0800

    test3

--- configs/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  helm_version = {
-    trivy         = "0.20.6" // renovate: dep=trivy-operator chart=https://aquasecurity.github.io/helm-charts/
-    argo_rollouts = "2.34.1" // renovate: dep=argo-rollouts chart=https://argoproj.github.io/argo-helm
-  }
-}
