6d637e0fa875ac6698b18ddf107e1772868e8b5d
Priscilla Celine Setiawan <priscilla.setiawan@tomoloyalty.com>
2024-04-23 20:23:03 +0800

    test

--- config/config.js
+++ config/config.js
@@ -1,53 +1,53 @@
-module.exports = {
-    requireConfig: "optional",
-    onboarding: false,
-    branchPrefix: 'update-version/',
-    username: 'renovate-release',
-    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    platform: 'github',
-    includeForks: true,
-    prHourlyLimit: 0,
-    prConcurrentLimit: 0,
-    packageRules: [
-        {
-            description: 'lockFileMaintenance',
-            matchUpdateTypes: [
-                'pin',
-                'digest',
-                'patch',
-                'minor',
-                'major',
-                'lockFileMaintenance',
-            ],
-            dependencyDashboardApproval: false,
-            automerge: false,  // enable auto merge for patch and minor update 
-            automergeType: "branch"
-        }
-    ],
+// module.exports = {
+//     requireConfig: "optional",
+//     onboarding: false,
+//     branchPrefix: 'update-version/',
+//     username: 'renovate-release',
+//     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+//     platform: 'github',
+//     includeForks: true,
+//     prHourlyLimit: 0,
+//     prConcurrentLimit: 0,
+//     packageRules: [
+//         {
+//             description: 'lockFileMaintenance',
+//             matchUpdateTypes: [
+//                 'pin',
+//                 'digest',
+//                 'patch',
+//                 'minor',
+//                 'major',
+//                 'lockFileMaintenance',
+//             ],
+//             dependencyDashboardApproval: false,
+//             automerge: false,  // enable auto merge for patch and minor update 
+//             automergeType: "branch"
+//         }
+//     ],
 
-    kubernetes: {
-        fileMatch: ["\\.yaml$"]
-    },
-    customManagers: [
-        {
-            customType: "regex",
-            datasourceTemplate: "helm",
-            fileMatch: [
-                "(^|/)locals\\.tf$"
-            ],
-            matchStrings: [
-                "trivy         = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "argo_rollouts = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "prometheus    = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "grafana                      = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "jenkins                      = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "kubevault-crds               = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "mongo-express                = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "datadog                      = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "cluster_autoscaler           = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "aws_load_balancer_controller = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-                "aws_cloudwatch_metrics       = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
-            ]
-        }
-    ],
-};
+//     kubernetes: {
+//         fileMatch: ["\\.yaml$"]
+//     },
+//     customManagers: [
+//         {
+//             customType: "regex",
+//             datasourceTemplate: "helm",
+//             fileMatch: [
+//                 "(^|/)locals\\.tf$"
+//             ],
+//             matchStrings: [
+//                 "trivy         = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "argo_rollouts = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "prometheus    = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "grafana                      = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "jenkins                      = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "kubevault-crds               = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "mongo-express                = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "datadog                      = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "cluster_autoscaler           = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "aws_load_balancer_controller = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//                 "aws_cloudwatch_metrics       = \"+(?<currentValue>[^'\" ]+)\" +\\/\\/ renovate: dep=(?<depName>[^\\s]+) chart=(?<registryUrl>[^ \\n]+)",
+//             ]
+//         }
+//     ],
+// };
