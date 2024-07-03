7695b638f815754fa0f5796a948825f2c9e6112f
priscillasetiawan-tomo <156643314+priscillasetiawan-tomo@users.noreply.github.com>
2024-07-03 10:45:52 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -56,7 +56,7 @@ module.exports = {
     requireConfig: "optional",
     onboarding: false,
     extends: [
-        "local>priscillasetiawan-tomo/renovate_test"
+        "github>priscillasetiawan-tomo/renovate_test"
     ],
 }
 
